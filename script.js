c4e4e52c76ad7bd8dc7e73020aaad2256425b652
Grady Peck <gradypeck717@gmail.com>
2019-11-06 12:28:40 -0500

    First attempt to commit program flow lab

---
+++ script.js
@@ -0,0 +1,35 @@
+let name = "Grady Peck";
+let age = 25;
+let birthday = "March 9th";
+let detroitGC = false;
+let lifeEvents = [
+    "I was born in Pontiac, Michigan.",
+    "I went to Hampshire College for about thirty seconds.",
+    "I was on a champion quiz bowl team in high school, which wasn't nerdy at all.",
+    "I just moved to Grand Rapids."
+]
+//opening statement
+if(detroitGC) {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`);
+}
+else {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`);
+}
+//print out the life events
+for(let i = 0; i < lifeEvents.length; i++){
+    console.log(lifeEvents[i]);
+}
+let counter = 0;
+let rando = 0;
+while(true){
+    rando = Math.round(Math.random()*10);
+    if(rando === 5){
+        counter ++;
+        console.log(`${rando} === 5. It took ${counter} iterations to randomly generate the number 5.`)
+        break;
+    }
+    else {
+        console.log(`${rando} !=== 5`);
+        counter++;
+    }
+}
